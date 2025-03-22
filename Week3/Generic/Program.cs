c987652582cac685f378a5644efb99cce74b5a0f
Abdelrahman Emad <abdoemad0120@gmail.com>
2025-03-22 07:50:10 +0200

    Add files via upload

---
+++ Week3/Generic/Program.cs
@@ -0,0 +1,65 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Diagnostics.Metrics;
+
+namespace Week3
+{
+    internal class Program
+    {
+
+        public static void swap<T>(ref T u,ref T x)
+        {
+            T temp = u;
+            u = x;
+            x = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            #region Create a generic method Swap<T> that swaps two variables of any type.
+
+            // int x = 9 , y = 5 ;
+            // Console.WriteLine($"before swap: x = {x} , y = {y}");
+            // swap(ref x,ref y);
+            // Console.WriteLine($"after swap: x = {x} , y = {y}");
+
+            // string s = "Abdelrahman";
+            //// swap(ref s, ref x); // cant swap string with int 
+            #endregion
+
+
+            #region Create a Dictionary<string, int> to store student names and their grades
+
+            //student student = new student();
+
+            //student.add("Abdelrahman emad", 4);
+            //student.add("sara ziad", 3.5f);
+            //student.add("mostafa yossef", 2.3f);
+            //student.add("eslam emad", 4.6f);
+
+            //student.display("Abdelrahman emad");
+
+            //student.display("mohaned");
+
+            #endregion
+
+
+            #region Implement a Queue<string> to simulate a customer service line
+
+            //Queue<string> customer = new Queue<string>();
+
+            //customer.Enqueue("Abdelrahman emad");
+            //customer.Enqueue("mostafa");
+            //customer.Enqueue("ziad");
+            //customer.Enqueue("mona");
+
+            //while (customer.Count > 0)
+            //{
+                
+            //   Console.WriteLine($"Serving... customer: {customer.Dequeue()}");
+            //}
+
+            #endregion
+        }
+
+    }
+}
---
+++ Week3/Generic/student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3
+{
+    public class student
+    {
+        public Dictionary<string, float> students;
+        public student() => students = new Dictionary<string, float>();
+
+        public void add (string name, float grad)
+        {
+            students.Add(name, grad); 
+        }
+
+        public float find(string name)
+        {
+            return students.ContainsKey(name) ? students[name] : -1;
+        }
+
+        public void display(string name)
+        {
+            if(students.ContainsKey(name))
+            {
+                Console.WriteLine($"{name} => {students[name]}");
+            }
+           else
+            {
+                Console.WriteLine("student does not exsist");
+            }
+        }
+    }
+}
